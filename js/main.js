3ec671bbdccfd5b69019401bf1950c08d3cce99b
QNhat1998 <46757841+QNhat1998@users.noreply.github.com>
2022-09-22 06:55:50 +0700

    end project Meipaly

---
+++ js/main.js
@@ -0,0 +1,51 @@
+var btn = $('.backToTop');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
+
+$(window).on("scroll", function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 70) {
+        $("#header-scroll").addClass("nav-fixed");
+    } else {
+        $("#header-scroll").removeClass("nav-fixed");
+    }
+});
+
+$(document).ready(function () {
+    $(".counter").each(function () {
+        var count = $(this);
+        var countTo = count.attr('data-count');
+        // console.log(countTo);
+        $({ countNum: count.text() }).animate({
+            countNum: countTo,
+        },
+            {
+
+                duration: 2000,
+                easing: 'linear',
+                step: function () {
+                    count.text(Math.floor(this.countNum));
+                },
+                complete: function () {
+                    count.text(this.countNum);
+                }
+            });
+    });
+});
+
+document.getElementById("btn-switch").onclick = function () {
+    document.getElementById("darkMode").classList.toggle("dark-mode");
+    document.getElementById("btn-switch").classList.toggle("dark-theme");
+}
